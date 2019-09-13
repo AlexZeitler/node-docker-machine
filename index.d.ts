65a6aa66697eef1acbd9a1380fd4054511f62bf7
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2019-09-13 16:09:11 +0200

    add types for machine

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+export declare class Machine {
+    name: string;
+    responseTime: number;
+    state: any;
+    activeHost: boolean;
+    activeSwarm: boolean;
+    dockerVersion: string;
+    constructor(opts: any);
+    static options(opts: any): any;
+    static command(args: any, done: any): void;
+    static status(name: string, done: any): void;
+    static isRunning(name: string, done: any): void;
+    static create(name: string, driver: any, options: any, done: any): void;
+    static start(name: string, done: any): void;
+    static stop(name: string, done: any): void;
+    static kill(name: string, done: any): void;
+    static env(name: string, opts: any, done: any): void;
+    static ssh(name: string, cmd: any, done: any): void;
+    static inspect(name: string, done: any): void;
+    static list(opts: any, done: any): void;
+}
